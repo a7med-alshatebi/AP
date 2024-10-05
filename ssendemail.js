2548a8876b300047552c8a1bcc8c3a9938487fee
Ahmed Alshatebi <154013344+a7med-alshatebi@users.noreply.github.com>
2024-10-05 17:54:29 +0300

    Update ssendemail.js

--- ssendemail.js
+++ ssendemail.js
@@ -1,13 +1,10 @@
-function sendEmail(){
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "ahmedalshatibi22@gmail.com",
-        Password : "Ahmed123456",
-        To : 'name@website.com',
-        From : "document.getElementById('email').value",
-        Subject : "New Contact Form Enquiry",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
+function sendMail(){
+    let parms = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value,
+
+    }
+    emailjs.send("service_q2lb6te","template_i03meq7",parms).then(alert('Email Sent Successfully'));
 }
